8168d534e52358d1e828adb08abee57d31a1f69a
Jossyb8512 <Joss-B@ulster.ac.uk>
2026-08-10 13:45:31 +0100

    Add initial Ukraine boundary visualisation

--- ukraine_conflict_analysis.py
+++ ukraine_conflict_analysis.py
@@ -1,3 +1,10 @@
 import geopandas as gpd
+import matplotlib.pyplot as plt
 oblasts = gpd.read_file('data/boundaries/geoBoundaries-UKR-ADM1-all/geoBoundaries-UKR-ADM1.geojson')
 print(oblasts.head())
+print(oblasts.columns)
+print(oblasts.crs)
+print(f"Number of administrative regions: {len(oblasts)}")
+print(oblasts['shapeName'].tolist())
+oblasts.plot()
+plt.show()
